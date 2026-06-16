77a67671c6fbf6c170af3547bf283a050b69e002
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-06-15 20:56:34 -0400

    style Header component and integrate global styles and assets

--- src/app/header.ts
+++ src/app/header.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.html',
+  styleUrl: './header.css',
   // standalone: true,
 })
 export class Header { }
