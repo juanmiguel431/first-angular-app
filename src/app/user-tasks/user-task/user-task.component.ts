1e3f2b183cf72d6158bbc1b93111c02ce76e8841
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-06-17 21:14:46 -0400

    feat(UserTasks): add UserTask component and integrate into UserTasks template

---
+++ src/app/user-tasks/user-task/user-task.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-task',
+  imports: [],
+  templateUrl: './user-task.component.html',
+  styleUrl: './user-task.component.css',
+})
+export class UserTask {}
--- src/app/user-tasks/user-tasks.component.ts
+++ src/app/user-tasks/user-tasks.component.ts
@@ -1,8 +1,9 @@
 import { Component, input } from '@angular/core';
+import { UserTask } from './user-task/user-task.component';
 
 @Component({
   selector: 'app-user-tasks',
-  imports: [],
+  imports: [UserTask],
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
