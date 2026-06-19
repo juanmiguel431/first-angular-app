7013902eb78e86ef26f16522cca38ed4d509e8e8
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-06-19 19:02:45 -0400

    refactor(UserTasks): extract DUMMY_TASKS to shared constants file for reusability

---
+++ src/app/constants/index.ts
@@ -0,0 +1,29 @@
+import type { Task } from '../models';
+
+
+export const DUMMY_TASKS: Task[] = [
+  {
+    id: 't1',
+    userId: 'u1',
+    title: 'Master Angular',
+    summary: 'Learn all the basic and advanced features of Angular & how to apply them.',
+    dueDate: '2025-12-31',
+    completed: false,
+  },
+  {
+    id: 't2',
+    userId: 'u3',
+    title: 'Build first prototype',
+    summary: 'Build a first prototype of the online shop website',
+    dueDate: '2024-05-31',
+    completed: false,
+  },
+  {
+    id: 't3',
+    userId: 'u3',
+    title: 'Prepare issue template',
+    summary: 'Prepare and describe an issue template which will help with project management',
+    dueDate: '2024-06-15',
+    completed: false,
+  },
+];
