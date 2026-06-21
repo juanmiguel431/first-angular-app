8fb6a951482c8aea360d2d3d2a73bb054fe2e0de
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-06-20 20:37:14 -0400

    refactor(AppModule): move Card to SharedModule and update imports accordingly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { UserTask } from './user-tasks/user-task/user-task.component';
 import { UserTaskForm } from './user-tasks/user-task-form/user-task-form';
 import { FormsModule } from '@angular/forms';
-import { Card } from './shared/card/card';
-import { DatePipe, NgOptimizedImage } from '@angular/common';
+import { NgOptimizedImage } from '@angular/common';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
-  declarations: [AppComponent, Header, UserTasks, UserTaskForm, Card, UserTask, UserComponent],
+  declarations: [AppComponent, Header, UserTasks, UserTaskForm, UserTask, UserComponent],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, FormsModule, NgOptimizedImage],
+  imports: [BrowserModule, FormsModule, NgOptimizedImage, SharedModule],
 })
 export class AppModule {}
